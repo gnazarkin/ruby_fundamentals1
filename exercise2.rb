afbd915b0462e88fb5fcf2e4e1604d1ea68aadef
gnazarkin <gleb.nazarkin@gmail.com>
2014-06-03 12:05:45 -0400

    second exercise is completed now

---
+++ exercise2.rb
@@ -0,0 +1,5 @@
+puts "A good tip on a 55 dollar mean is #{55*0.15}" #problem 1
+puts "I am " + 55.to_s + " years old" # problem 2
+puts "The result is #{45628*7839}"	# problem 3
+puts (true && false) || (false && true) || !(false && false) #problem 4, the solution is true
+
